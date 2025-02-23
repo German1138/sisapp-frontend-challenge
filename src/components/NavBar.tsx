5fb16eb535266ea18f80bc7b80a8dca49b37c651
German1138 <germancourvoisier1138@gmail.com>
2025-02-23 17:59:48 -0300

    feat: :recycle: Add Styled components
    Also make them semantically correct

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
+import { Badge, NavContainer } from "./NavBar.styles";
+
 import { CartContext } from "../Context/contexts";
 import { Link } from "react-router";
-import { colors } from "../theme.style";
 import { useContext } from "react";
 
 function NavBar() {
@@ -13,45 +14,25 @@ function NavBar() {
   const { cart } = context;
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        position: "fixed",
-        width: "100%",
-        padding: "10px",
-        backgroundColor: colors.blue,
-      }}
-    >
-      <Link to="/">
-        <h3 style={{ margin: 0 }}>SISAPP STORE</h3>
-      </Link>
+    <header>
+      <NavContainer>
+        <Link to="/">
+          <h1 style={{ fontSize: "24px" }}>SISAPP STORE</h1>
+        </Link>
 
-      <input></input>
+        <form role="search">
+          <input type="text" placeholder="Search..." aria-label="Search" />
+          <button type="submit">🔍</button>
+        </form>
 
-      <Link to="/cart">
-        <div style={{ position: "relative" }}>
-          <img src="src\assets\shopping-cart.svg" alt="shopping cart" />
-          {cart.length > 0 && (
-            <span
-              style={{
-                position: "absolute",
-                right: -5,
-                fontWeight: "700",
-                backgroundColor: colors.white,
-                padding: "3px",
-                borderRadius: "100%",
-                fontSize: "8px",
-                bottom: 0,
-              }}
-            >
-              {cart.length}
-            </span>
-          )}
-        </div>
-      </Link>
-    </div>
+        <Link to="/cart">
+          <div style={{ position: "relative" }}>
+            <img src="src\assets\shopping-cart.svg" alt="shopping cart" />
+            {cart.length > 0 && <Badge>{cart.length}</Badge>}
+          </div>
+        </Link>
+      </NavContainer>
+    </header>
   );
 }
 
