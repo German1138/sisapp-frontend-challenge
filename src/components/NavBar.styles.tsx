71ff9701f781d60ad98832caf6e859fd0880b29f
German1138 <germancourvoisier1138@gmail.com>
2025-02-23 18:00:09 -0300

    style: :lipstick: NavBar styled components

---
+++ src/components/NavBar.styles.tsx
@@ -0,0 +1,23 @@
+import { colors } from "../theme.style";
+import styled from "styled-components";
+
+export const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  padding: 15px;
+  background-color: ${colors.blue};
+`;
+
+export const Badge = styled.span`
+  position: absolute;
+  font-weight: 700;
+  background-color: ${colors.white};
+  padding: 3px;
+  border-radius: 100%;
+  font-size: 8px;
+  bottom: 0;
+`;
