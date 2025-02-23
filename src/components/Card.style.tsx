d3d267af2d175cf1f70d0d9e01042579a741e7f8
German1138 <germancourvoisier1138@gmail.com>
2025-02-23 16:10:10 -0300

    style: :lipstick: Card styled components file

---
+++ src/components/Card.style.tsx
@@ -0,0 +1,39 @@
+import { colors } from "../theme.style";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border-radius: 8px;
+  background-color: ${colors.white};
+  padding: 15px;
+  max-width: 250px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CardImage = styled.img`
+  object-fit: contain;
+  width: 100%;
+  height: 300px;
+`;
+
+export const SubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 10px;
+`;
+
+export const Title = styled.h3`
+  font-weight: 500;
+  font-size: 14px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin: 0;
+`;
+
+export const Price = styled.p`
+  font-size: 24px;
+  margin: 0;
+`;
