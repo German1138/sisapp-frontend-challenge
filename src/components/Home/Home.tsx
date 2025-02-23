c372daf5dc437409624ae638568ab55bac8507c8
German1138 <germancourvoisier1138@gmail.com>
2025-02-23 18:25:59 -0300

    feat: :recycle: Add styled components and semantic menaning

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,24 @@
+import { Container, SubContainer } from "./Home.styles";
+
+import Card from "../Card";
+import { IProduct } from "../../interfaces";
+import ProductsContext from "../../Context/ContextDeclarations";
+import { useContext } from "react";
+
+function Home() {
+  const products = useContext(ProductsContext);
+
+  return (
+    <Container>
+      <SubContainer>
+        {products.map((item: IProduct) => (
+          <article key={item.id} role="article">
+            <Card product={item} />
+          </article>
+        ))}
+      </SubContainer>
+    </Container>
+  );
+}
+
+export default Home;
