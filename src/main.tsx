aad48df1ae38faec2bff4bc7cd29483b25112787
German1138 <germancourvoisier1138@gmail.com>
2025-02-23 18:56:00 -0300

    refactor: :truck: Upgraded path of Product Detail

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import {
 import App from "./App";
 import Cart from "./components/Cart";
 import Layout from "./components/Layout";
-import ProductDetail from "./components/ProductDetail";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
