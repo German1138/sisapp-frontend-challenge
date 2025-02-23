56bb2afb0c6dd4ffdb8078ddf18ffb2aaf5a9e0b
German1138 <germancourvoisier1138@gmail.com>
2025-02-23 16:09:46 -0300

    style: :art: Removed styles from file
    Replaced them with Styled Components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,52 +1,19 @@
+import { CardImage, Container, Price, SubContainer, Title } from "./Card.style";
+
 import { ICard } from "../interfaces";
 import { Link } from "react-router";
-import { colors } from "../theme.style";
 
 function Card({ id, image, title, price }: ICard) {
   return (
     <Link to={`/${id}`}>
-      <div
-        style={{
-          borderRadius: "8px",
-          backgroundColor: colors.white,
-          padding: "15px",
-          maxWidth: "250px",
-          height: "400px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-        }}
-      >
-        <img
-          src={image}
-          style={{
-            objectFit: "contain",
-            width: "100%",
-            height: "300px",
-          }}
-        />
+      <Container>
+        <CardImage src={image} alt={title} />
 
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            gap: "10px",
-          }}
-        >
-          <p
-            style={{
-              fontSize: "14px",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              margin: "0",
-            }}
-          >
-            {title}
-          </p>
-          <span style={{ fontSize: "24px" }}>$ {price}</span>
-        </div>
-      </div>
+        <SubContainer>
+          <Title>{title}</Title>
+          <Price>$ {price}</Price>
+        </SubContainer>
+      </Container>
     </Link>
   );
 }
