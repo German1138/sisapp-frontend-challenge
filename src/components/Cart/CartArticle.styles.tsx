f66d94549c4e4f2b30ea8eda5aa76202a21d11a8
German1138 <germancourvoisier1138@gmail.com>
2025-02-25 13:57:14 -0300

    fix: :iphone: Fully responsive layout

--- src/components/Cart/CartArticle.styles.tsx
+++ src/components/Cart/CartArticle.styles.tsx
@@ -2,14 +2,33 @@ import { colors } from "../../theme.style";
 import styled from "styled-components";
 
 export const Container = styled.article`
-  border-radius: 8px;
+  border-radius: 5px;
   background-color: ${colors.white};
   padding: 15px;
   display: flex;
   gap: 20px;
   justify-content: space-between;
-  align-items: center;
   height: 120px;
+  align-items: flex-start;
+
+  @media (max-width: 320px) {
+    flex-direction: column;
+    position: relative;
+    height: auto;
+    align-items: center;
+  }
+`;
+
+export const ImageAndTitleContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px;
+  width: 100%;
+
+  @media (max-width: 320px) {
+    flex-direction: column;
+    gap: 0px;
+  }
 `;
 
 export const Image = styled.img`
@@ -17,6 +36,10 @@ export const Image = styled.img`
   height: 120px;
   max-width: 75px;
   width: 100%;
+
+  @media (max-width: 320px) {
+    max-width: none;
+  }
 `;
 
 export const SubContainer = styled.article`
@@ -34,27 +57,44 @@ export const SubContainer = styled.article`
 
 export const Title = styled.p`
   font-size: 18px;
+  margin-top: 20px;
+  height: 90px;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
   overflow: hidden;
-  max-width: 500px;
-  margin: 0;
+  -webkit-line-clamp: 4;
+  text-overflow: ellipsis;
 
   @media (max-width: 425px) {
-    font-size: 15px;
+    font-size: 13px;
+    height: 80px;
+    -webkit-line-clamp: 5;
+  }
+
+  @media (max-width: 320px) {
+    height: fit-content;
+    -webkit-line-clamp: 5;
   }
 `;
 
 export const ButtonsContainer = styled.div`
   display: flex;
+  flex-direction: column-reverse;
   gap: 20px;
-  max-width: 280px;
+  max-width: fit-content;
   width: 100%;
   justify-content: space-between;
+  align-items: end;
 
-  @media (max-width: 425px) {
+  @media (max-width: 768px) {
     width: fit-content;
-    flex-direction: column;
     justify-content: space-between;
+  }
+
+  @media (max-width: 320px) {
     align-items: center;
+    height: auto;
+    flex-direction: row-reverse;
   }
 `;
 
@@ -64,7 +104,7 @@ export const StockContainer = styled.div`
   width: 100%;
   max-width: 80px;
   border: 2px solid ${colors.blue};
-  border-radius: 8px;
+  border-radius: 5px;
   align-items: center;
 `;
 
@@ -78,13 +118,12 @@ export const StockButton = styled.button`
 
   &:hover {
     color: ${colors.blue};
-    transform: scale(1.05);
+    transform: scale(1.02);
   }
 `;
 
 export const Price = styled.span`
   font-size: 24px;
-  width: 115px;
   text-align: center;
 
   @media (max-width: 425px) {
@@ -96,21 +135,28 @@ export const Price = styled.span`
 export const DeleteAllButton = styled.button`
   all: unset;
   cursor: pointer;
-  padding: 5px 10px;
+  width: 20px;
+  height: 20px;
+  text-align: center;
   font-weight: 600;
   background-color: ${colors.red};
   color: ${colors.white};
-  border-radius: 8px;
-  width: fit-content;
+  border-radius: 50px;
+  font-size: 13px;
 
-  transition: background-color 0.3s ease-in-out, transform 0.2s ease;
+  transition: background-color 150ms ease-in-out, transform 150ms ease;
 
   &:hover {
     background-color: ${colors.alt_red};
-    transform: scale(1.05);
   }
 
   &:focus {
     outline: 2px solid ${colors.red};
+  }
+
+  @media (max-width: 320px) {
+    position: absolute;
+    top: 10px;
+    right: 10px;
   }
 `;
