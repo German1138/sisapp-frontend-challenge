7b1a28d0565b2b253854cf7ae2a3f0ad8fbf56bd
German1138 <germancourvoisier1138@gmail.com>
2025-02-24 23:59:27 -0300

    style: :iphone: Added media queries for better experience in mobile

--- src/components/Home/Home.styles.tsx
+++ src/components/Home/Home.styles.tsx
@@ -10,5 +10,12 @@ export const SubContainer = styled.section`
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 30px;
   max-width: 1024px;
-  padding: 100px 0px;
+  padding: 100px 10px;
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 15px;
+  }
 `;
