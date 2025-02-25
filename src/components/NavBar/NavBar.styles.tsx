ba80c1b4d9fa1822d431cf08ef0523261791c07e
German1138 <germancourvoisier1138@gmail.com>
2025-02-25 00:01:58 -0300

    style: :lipstick: Added transition to improve accesibility and feedback

--- src/components/NavBar/NavBar.styles.tsx
+++ src/components/NavBar/NavBar.styles.tsx
@@ -8,8 +8,28 @@ export const NavContainer = styled.nav`
   position: fixed;
   top: 0;
   width: 100%;
-  padding: 15px;
+  z-index: 1500;
+  padding: 15px 0;
   background-color: ${colors.blue};
+`;
+
+export const Logo = styled.h1`
+  font-size: 24px;
+  transition: background-color 0.3s ease-in-out, transform 0.2s ease;
+
+  &:hover {
+    color: ${colors.white};
+    transform: scale(1.05);
+  }
+`;
+
+export const CartLogo = styled.div`
+  position: relative;
+  transition: background-color 0.3s ease-in-out, transform 0.2s ease;
+
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
 
 export const Badge = styled.span`
@@ -20,4 +40,11 @@ export const Badge = styled.span`
   border-radius: 100%;
   font-size: 8px;
   bottom: 0;
+  right: -3px;
+
+  transition: background-color 0.3s ease-in-out, transform 0.2s ease;
+
+  &:hover {
+    transform: scale(1.25);
+  }
 `;
